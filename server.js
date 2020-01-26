3296f71aa317de6089a12df897370db4269479d1
Mohammad Kafee Uddin <kafee651@users.noreply.github.com>
2020-01-26 21:33:21 +0530

    added style get api
    added style get api

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/api/getproducts/id/:id',(req, res) => {
 
 //GET PRODUCT PRICE by PRODUCT_ID ... To retrieve product price call this API ... URL/api/getproducts/price/'Product_id'
 app.get('/api/getproducts/price/:id',(req, res) => {
-  let sql = "SELECT ITEM_NUMBER,LIST_PRICE FROM XXIBM_PRODUCT_PRICING WHERE ITEM_NUMBER="+req.params.id;
+  let sql = "SELECT * FROM XXIBM_PRODUCT_PRICING WHERE ITEM_NUMBER="+req.params.id;
   console.log(sql);
   let query = mysqlClient.query(sql, (err, results) => {
     if(err) throw err;
@@ -104,14 +104,8 @@ app.get('/api/getproducts/price/:id',(req, res) => {
 });
 
 //GET PRODUCT PRICE by PRODUCT_ID ... To retrieve product price call this API ... URL/api/getproducts/price/'Product_id'
-app.get('/api/getproducts/detailsprice/:id',(req, res) => {
-  let sql = "SELECT PRODUCT.ITEM_NUMBER AS ITEM_NUMBER,PRODUCT.DESCRIPTION AS DESCRIPTION,PRODUCT.LONG_DESCRIPTION AS LONG_DESCRIPTION,"+
-  "PRODUCT.CATALOGUE_CATEGORY AS CATALOGUE_CATEGORY,PRODUCT.SKU_UNIT_OF_MEASURE AS SKU_UNIT_OF_MEASURE,"+
-  "PRODUCT.STYLE_ITEM AS STYLE_ITEM,PRODUCT.SKU_ATTRIBUTE1 AS SKU_ATTRIBUTE1,PRODUCT.SKU_ATTRIBUTE2 AS SKU_ATTRIBUTE2,"+
-  "PRODUCT.SKU_ATTRIBUTE_VALUE1 AS SKU_ATTRIBUTE_VALUE1, PRODUCT.SKU_ATTRIBUTE_VALUE2 AS SKU_ATTRIBUTE_VALUE2,"+
-  "PRICING.LIST_PRICE AS LIST_PRICE,PRICING.INSTOCK AS INSTOCK,PRICING.DISCOUNT AS DISCOUNT FROM XXIBM_PRODUCT_SKU"+ 
-  "AS PRODUCT JOIN XXIBM_PRODUCT_PRICING  AS PRICING"+
-  "ON PRODUCT.ITEM_NUMBER = PRICING.ITEM_NUMBER";// WHERE PRODUCT.ITEM_NUMBER="+req.params.id;
+app.get('/api/getproducts/style/:id',(req, res) => {
+  let sql = "SELECT * FROM XXIBM_PRODUCT_STYLE WHERE ITEM_NUMBER="+req.params.id;// WHERE PRODUCT.ITEM_NUMBER="+req.params.id;
   console.log(sql);
   let query = mysqlClient.query(sql, (err, results) => {
     if(err) throw err;
